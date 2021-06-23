a592d11c7a2b2a80fc255a49a5aa5b003290bf61
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-22 21:33:50 -0300

    Lógica

---
+++ Opposites Attract.js
@@ -0,0 +1,31 @@
+/* Timmy & Sarah think they are in love, but around where they live, 
+they will only know once they pick a flower each. If one of the flowers has an
+even number of petals and the other has an odd number of petals it means they are in
+love.
+
+Write a function that will take the number of petals of each flower and return true
+if they are in love and false if they aren't. */
+
+function lovefunc(f1, f2){
+    if(f1 % 2 == 0 && f2 % 2 == 1){
+        return true
+    }else if (f1 % 2 == 1 && f2 % 2 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+
+
+//Alternative
+function love(flower1, flower2){
+    return flower1 % 2 !== flower2 % 2;
+  }
+
+
+//Alternative
+function loveF(flower1, flower2){
+    return (flower1 + flower2) % 2 == 1;
+  }
+
