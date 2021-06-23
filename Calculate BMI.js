bac66af3e41eb85c8cd09de0769848c33dbb49c5
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-22 21:47:55 -0300

    Operador ternário

---
+++ Calculate BMI.js
@@ -0,0 +1,23 @@
+/* Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese" */
+
+function bmi(weight, height) {
+    const imc = weight / ( height * height);
+    return imc <= 18.5 ? "Underweight" : imc <= 25 ? "Normal" : imc <= 30 ? "Overweight" : "Obese";
+  }
+
+
+//Alternative
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
+
+
+//Alternative
