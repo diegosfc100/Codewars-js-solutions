7aee02901c2267da99dc30f39340e48e12114b84
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-28 18:55:10 -0300

    toString( ) / split( ) / map( ) / parseInt( )

---
+++ Convert number to reversed array of digits.js
@@ -0,0 +1,23 @@
+/* Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3] */
+
+function digitize(n) {
+    let array = n.toString().split("");
+      return array.map(x => parseInt(x)).reverse();
+  }
+
+
+
+//Alternative
+function digitize(n) {
+    return String(n).split('').map(Number).reverse()
+  }
+
+
+//Alternative
+function digitize(n) {
+    return Array.from(String(n), Number).reverse();
+  }
