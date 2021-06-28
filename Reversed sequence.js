8bca6cdcb5e2f2783bce155cab0d95302b949413
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-28 19:01:38 -0300

    push( ) / for / fill( ) / map( ) / Array.from( )

---
+++ Reversed sequence.js
@@ -0,0 +1,30 @@
+/* Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+
+ */
+
+const reverseSeq = n => {
+    let arr = [];
+    for(let i = n; i > 0; i--){
+        arr.push(i);
+    }
+     return arr;
+  };
+
+
+
+//Alternative
+const reverseSeq = num => {
+    return new Array(num)
+        .fill()
+        .map((d, i) => i + 1)
+        .reverse()
+  };
+
+
+//Alternative
+const reverseSeq = length => Array.from({length}, () => length--)
+
+
+
