77e61951b54815fae077330b40f2bb7d2d7b0000
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-06 19:48:34 -0300

    Lógica

---
+++ I love you, a little , a lot, passionately ... not at all.js
@@ -0,0 +1,39 @@
+/* Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, 
+saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals,
+where nb_petals > 0. */
+
+function howMuchILoveYou(P) {
+    const arr = ['not at all','I love you','a little','a lot','passionately','madly']
+    P = P % 6
+    return arr[P]
+  }
+
+
+//Alternative
+const howMuchILoveYou=n=>['not at all','I love you','a little','a lot','passionately','madly'][n%6]
+
+
+//alternative
+function howMuchILoveYou(nbPetals) {
+    let phrase = {    
+      0: "not at all",
+      1: "I love you",
+      2: "a little",
+      3: "a lot",
+      4: "passionately",
+      5: "madly"
+    }
+    return phrase[nbPetals%6]
+  }
+
+  
