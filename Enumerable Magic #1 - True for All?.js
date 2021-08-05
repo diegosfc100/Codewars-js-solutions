e46638d38859914c4eda73b51e261e4164b34eed
diegosfc100 <diego.sfc100@hotmail.com>
2021-08-04 22:18:27 -0300

    every( )

---
+++ Enumerable Magic #1 - True for All?.js
@@ -0,0 +1,14 @@
+/*
+Create a method all which takes an array and a predicate (function pointer), 
+and returns true if the predicate returns true for every element in the array.
+Otherwise, it should return false. If the array is empty, it should return true, 
+since technically nothing failed the test.*/
+
+
+function all( arr, fun ){
+    return arr.every(fun)
+  }
+
+console.log(all([1,2,3,4,5], function(v){return v>9}))
+
+
